e71c803a0b148301ed5be1aea5e4f54ca273f9e7
patchthesock <stephencallaghan91@gmail.com>
2014-12-06 17:13:12 +0000

    Added Timer.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public float timer = 0;
+
+	void Start ()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(GameObject.FindGameObjectWithTag("Player"))
+		{
+			timer += Time.deltaTime;
+		}
+	}
+}

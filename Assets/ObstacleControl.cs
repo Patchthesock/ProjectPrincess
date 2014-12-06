58a1b1c5ddd9302790a86c9cb2868e2b82c0de79
patchthesock <stephencallaghan91@gmail.com>
2014-12-06 13:04:57 +0000

    Code

---
+++ Assets/ObstacleControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleControl : MonoBehaviour
+{
+	public float speed = 1.0f;
+
+	void Update ()
+	{
+		this.transform.Translate(-Vector3.right * speed);
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] obstacles;
+	public float spawnRate = 1.0f;
+
+	private float timer = 0.0f;
+
+	void Update ()
+	{
+		timer += Time.deltaTime;
+
+		if(timer >= spawnRate)
+		{
+			int random = Random.Range(0, obstacles.Length-1);
+			Instantiate(obstacles[random], this.transform.position, this.transform.rotation);
+			timer = 0f;
+		}
+	}
+}

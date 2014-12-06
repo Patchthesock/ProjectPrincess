1efa8cf4b9aded778646e15896b19b9fc13e0916
patchthesock <stephencallaghan91@gmail.com>
2014-12-06 14:40:54 +0000

    Physics Update

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,18 +8,28 @@ public class Spawner : MonoBehaviour {
 	public float maxSpawnRate = 3.0f;
 
 	private float timer = 0.0f;
+	private float actualSpawnRate = 0.0f;
+
+	private float SetSpawnRate()
+	{
+		return Random.Range(minSpawnRate, maxSpawnRate);
+	}
+
+	void Start ()
+	{
+		actualSpawnRate = SetSpawnRate();
+	}
 
 	void Update ()
 	{
 		timer += Time.deltaTime;
-		float actualSpawnRate = Random.Range(minSpawnRate, maxSpawnRate);
-
 		if(timer >= actualSpawnRate)
 		{
 			int random = Random.Range(0, obstacles.Length);
 			float offsetAngle = Random.Range(0, 45);
 			GameObject newObstacle = (GameObject)Instantiate(obstacles[random], this.transform.position, this.transform.rotation*Quaternion.Euler(0,offsetAngle,0));
 			Destroy(newObstacle, 5f);
+			actualSpawnRate = SetSpawnRate();
 			timer = 0f;
 		}
 	}
